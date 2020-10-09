4d9e5ea58cd6df2924ccac796eb17559e087948b
Alberto <arodriguezg@iesch.org>
2020-10-09 16:52:00 +0200

    Aplicacion Finalizada

--- app/src/main/java/org/iesch/alberto/a5_radiobutton/MainActivity.java
+++ app/src/main/java/org/iesch/alberto/a5_radiobutton/MainActivity.java
@@ -3,6 +3,7 @@ package org.iesch.alberto.a5_radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -18,28 +19,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView texto = (TextView) findViewById(R.id.texto);
-        RadioButton rb1 = (RadioButton) findViewById(R.id.rb1);
-        RadioButton rb2 = (RadioButton) findViewById(R.id.rb2);
-        RadioButton rb3 = (RadioButton) findViewById(R.id.rb3);
-        RadioButton rb4 = (RadioButton) findViewById(R.id.rb4);
+        final TextView texto = (TextView) findViewById(R.id.texto);
+        final RadioButton rb1 = (RadioButton) findViewById(R.id.rb1);
+        final RadioButton rb2 = (RadioButton) findViewById(R.id.rb2);
+        final RadioButton rb3 = (RadioButton) findViewById(R.id.rb3);
+        final RadioButton rb4 = (RadioButton) findViewById(R.id.rb4);
 
         Button boton = (Button) findViewById(R.id.boton);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( rb1.isChecked()){
+                    texto.setText(" Tu coche favorito es el Ferrari F40");
+                }
+                if ( rb2.isChecked()){
+                    texto.setText(" Tu coche favorito es el Ferrari ENZO");
+                }
+                if ( rb3.isChecked()){
+                    texto.setText(" Tu coche favorito es el Ferrari FXXK");
+                }
+                if ( rb4.isChecked()){
+                    texto.setText(" Tu coche favorito es el Ferrari 321GTO");
+                }
+            }
+        });
     }
 
-    public void pulsarBoton(){
 
-        if ( rb1.isChecked()){
-            texto.setText(" Tu coche favorito es el Ferrari F40");
-        }
-        if ( rb2.isChecked()){
-            texto.setText(" Tu coche favorito es el Ferrari ENZO");
-        }
-        if ( rb3.isChecked()){
-            texto.setText(" Tu coche favorito es el Ferrari FXXK");
-        }
-        if ( rb4.isChecked()){
-            texto.setText(" Tu coche favorito es el Ferrari 321GTO");
-        }
-    }
 }
--- app/src/main/java/org/iesch/alberto/a5_radiobutton/SplashScreen.java
+++ app/src/main/java/org/iesch/alberto/a5_radiobutton/SplashScreen.java
@@ -13,6 +13,7 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,6 +21,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },4000);
     }
 }
